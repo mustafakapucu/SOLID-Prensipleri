805757d25d86c53a031297577b74c05470a47e89
mustafakapucu <mustafakapucu@windowslive.com>
2018-10-26 17:02:39 +0300

    #DIP
    Dependency Injection

---
+++ SOLID/DependencyInversion/Problem/DIP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.DependencyInversion.Problem
+{
+
+    public class FileLogger
+    {
+        public string Message { get; set; }
+
+        public void Log()
+        {
+
+        }
+    }
+
+    public class Dbloger
+    {
+        public string Message { get; set; }
+
+        public void Log()
+        {
+
+
+
+        }
+    }
+
+    public class LogManager
+    {
+        private FileLogger _file;
+        private Dbloger _db;
+
+        public LogManager()
+        {
+            _file = new FileLogger();
+            _db = new Dbloger();
+        }
+
+        public void Log()
+        {
+            _file.Log();
+            _db.Log();
+        }
+
+    }
+
+
+
+}
---
+++ SOLID/DependencyInversion/Solution/DIP.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.DependencyInversion.Solution
+{
+    public class DIP
+    {
+        public static void Main()
+        {
+            var dbLogger = new DBLogger();
+            dbLogger.Message = "Mesajjj";
+
+            var manager = new LogManager(dbLogger);
+            manager.Log();
+        }
+    }
+
+
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class FileLogger : ILogger
+    {
+        public string Message { get; set; }
+        public void Log()
+        {
+            //File Log
+        }
+    }
+
+    public class DBLogger : ILogger
+    {
+        public string Message { get; set; }
+
+        public void Log()
+        {
+            //Database Log
+        }
+    }
+
+    public class LogManager
+    {
+
+        private ILogger _logger;
+
+        public LogManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Log()
+        {
+            _logger.Log();
+        }
+    }
+
+
+
+}
