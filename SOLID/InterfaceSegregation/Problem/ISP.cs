7d86a374464ddd15adddceadf1b24da7bb008d37
mustafakapucu <mustafakapucu@windowslive.com>
2018-10-26 15:40:15 +0300

    #ISP
    Interfage Segregation

---
+++ SOLID/InterfaceSegregation/Problem/ISP.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.InterfaceSegregation.Problem
+{
+
+    interface IComponent
+    {
+        Guid ComponentId { get; }
+
+        void Initialize();
+        void Draw();
+        void Render();
+    }
+
+    public class WinButton : IComponent
+    {
+        public Guid ComponentId
+        {
+            get
+            {
+                return Guid.NewGuid();
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("Ekrana çizdirme işlemleri");
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Windows Button başlangıç işlemleri");
+        }
+
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class WebButon : IComponent
+    {
+        public Guid ComponentId
+        {
+            get
+            {
+                return Guid.NewGuid();
+            }
+        }
+
+        public void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Web Button başlangıç işlemleri");
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("HTML Render işlemleri");
+        }
+    }
+}
---
+++ SOLID/InterfaceSegregation/Solution/ISP.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.InterfaceSegregation.Solution
+{
+    interface IComponent
+    {
+        Guid ComponentId { get; }
+
+        void Initialize();
+    }
+
+    interface IWebComponent
+    {
+        void Render();
+    }
+
+    interface IWinComponent
+    {
+        void Draw();
+    }
+
+    public class WinButton : IComponent, IWinComponent
+    {
+        public Guid ComponentId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Draw()
+        {
+            Console.WriteLine("Ekrana çizdirme işlemleri");
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Windows Button başlangıç işlemleri");
+        }
+    }
+
+    public class WebButton : IComponent, IWebComponent
+    {
+        public Guid ComponentId
+        {
+            get { return Guid.NewGuid(); }
+        }
+
+        public void Initialize()
+        {
+            Console.WriteLine("Web Button başlangıç işlemleri");
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("HTML Render işlemleri");
+        }
+    }
+}
